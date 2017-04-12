0450c2120e47b10411147d641d4aa1fa9b470298
joazy <wallyceazy@gmail.com>
2017-04-11 22:12:57 -0300

    Adicionado novas funções
    Adicionado as funções de colisão com bloco para pouso, função de colisão
    com as bordas.

--- Sprite.js
+++ Sprite.js
@@ -1,24 +1,122 @@
 function Sprite(){
+  this.g = 0;
   this.x = 0;
   this.y = 0;
-  this.vx =0;
-  this.vy =0;
-  this.ax =0;
+  this.vx = 0;
+  this.vy = 0;
+  this.ax = 0;
   this.ay = 0;
+  this.am = 0;
+  this.width = 25;
+  this.height = 25;
+  this.angle = 0;
+  this.vang = 0;
   this.color = "blue";
+  this.cooldown = 0;
 }
 
 Sprite.prototype.desenhar = function (ctx) {
-  ctx.fillColor = this.color;
-  ctx.fillRect(this.x, this.y, 15,15);
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(this.x, this.y, 15,15);
+  ctx.save();
+  ctx.translate(this.x, this.y);
+  ctx.rotate(this.angle*2*Math.PI/360);
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.moveTo(-this.width/2, -this.height/2);
+  ctx.lineTo(-this.width/2, +this.height/2);
+  ctx.lineTo(+this.width/2, 0);
+  ctx.closePath();
+  ctx.fill();
+  ctx.restore();
+};
+
+Sprite.prototype.desenharBloco = function (ctx) {
+  ctx.save();
+  ctx.translate(this.x, this.y);
+  ctx.fillStyle = this.color;
+  ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height);
+  ctx.restore();
 };
 
 Sprite.prototype.mover = function (dt) {
-  
   this.vx = this.vx + this.ax*dt;
-  this.vy = this.vy + (this.ay)*dt;
+  this.vy = this.vy + (this.ay+this.g)*dt;
   this.x = this.x + this.vx*dt;
   this.y = this.y + this.vy*dt;
+  this.angle = this.angle + this.vang*dt;
+  if(this.cooldown>0) {
+    this.cooldown -= dt;
+  } else {
+    this.cooldown = 0;
+  }
 };
+
+Sprite.prototype.moverAng = function (dt) {
+  this.angle = this.angle + this.vang*dt;
+  this.ax = this.am*Math.cos(Math.PI*this.angle/180);
+  this.ay = this.am*Math.sin(Math.PI*this.angle/180);
+  this.vx = this.vx + this.ax*dt;
+  this.vy = this.vy + (this.ay+this.g)*dt;
+  this.x = this.x + this.vx*dt;
+  this.y = this.y + this.vy*dt;
+  if(this.cooldown>0) {
+    this.cooldown -= dt;
+  } else {
+    this.cooldown = 0;
+  }
+};
+Sprite.prototype.colidindo = function () {
+this.vy =0;
+this.vx=0;
+this.ax =0;
+this.ay=0;
+
+};
+Sprite.prototype.colidiuComBorda = function () {
+  if(this.x-this.width/2 <= 0 ){
+    this.vx=0;
+    this.ax=0;
+  }
+  if(this.x + this.width/2 >700){
+    this.vx=0;
+    this.ax=0;
+  }
+  if(this.y - this.width/2 < 0 ){
+    this.vy=0;
+    this.ay=0;
+  }
+  if(this.y + this.width/2 >300)
+  {
+    this.vy=0;
+    this.ay=0;
+  }
+};
+
+Sprite.prototype.colidiuCom = function (alvo) {
+  if(this.x+this.width < alvo.x) return false;
+  if(this.x > alvo.x+this.width) return false;
+  if(this.y+this.height < alvo.y) return false;
+  if(this.y > alvo.y+this.height) return false;
+  return true;
+};
+
+Sprite.prototype.perseguir = function (alvo, dt) {
+  this.ax = 10*dt*(alvo.x - this.x) - 0.05*this.vx;
+  this.ay = 10*dt*(alvo.y - this.y) - 0.05*this.vy;
+};
+
+Sprite.prototype.perseguirAng = function (alvo, dt) {
+  var dX = alvo.x - this.x;
+  var dY = alvo.y - this.y;
+  var dist = Math.sqrt(dX*dX+dY*dY);
+  var dA = 180*Math.acos(dX/dist)/Math.PI;
+  this.vang = 100*(dA - this.angle)*dt;
+};
+
+
+
+
+
+
+
+
+//
